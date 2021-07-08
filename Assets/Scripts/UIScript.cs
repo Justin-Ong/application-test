25aac20dc8149e5d467bf1daf13b125b75ce6e75
Justin-Ong <justinong32@gmail.com>
2021-07-08 17:09:12 +0800

    Add menu functionality Add scene changing/reloading functionality

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    AsyncOperation load;
+
+    public void RedSceneSelected()
+    {
+        PlayerController.instance.ChangeScene("RedScene");
+        DisplayLoadingScreen(load);
+    }
+
+    public void BlueSceneSelected()
+    {
+        PlayerController.instance.ChangeScene("BlueScene");
+        DisplayLoadingScreen(load);
+    }
+
+    public void GreenSceneSelected()
+    {
+        PlayerController.instance.ChangeScene("GreenScene");
+        DisplayLoadingScreen(load);
+    }
+
+    void DisplayLoadingScreen(AsyncOperation loadingScene)
+    {
+        Debug.Log("Loading");
+    }
+}
