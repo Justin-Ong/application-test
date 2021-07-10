39735f6a85803c4dd1861824e350f93ec545b92a
Justin-Ong <justinong32@gmail.com>
2021-07-10 16:10:11 +0800

    Move imported assets to correct folder

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIScript : MonoBehaviour
+public class MenuScript : MonoBehaviour
 {
     AsyncOperation load;
 
