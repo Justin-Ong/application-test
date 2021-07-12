3d8496fcdcf42d8fbca68789a5ef068a3026187e
Justin-Ong <justinong32@gmail.com>
2021-07-12 19:04:07 +0800

    Add controller to green scene to handle UI and starting/stopping of races

---
+++ Assets/Scripts/GreenSceneController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GreenSceneController : MonoBehaviour
+{
+    [Header("Racers")]
+    public List<GameObject> racers = new List<GameObject>();
+    public static List<Transform> racerTransforms = new List<Transform>();
+
+    [Header("UI")]
+    public Canvas canvas;
+    public Button startButton;
+
+    private int numRacers;
+    private int racersFinished;
+
+    void Start()
+    {
+        numRacers = racers.Count;
+        racersFinished = 0;
+        foreach (GameObject racer in racers)
+        {
+            racerTransforms.Add(racer.transform);
+        }
+    }
+
+    void Update()
+    {
+        if (racersFinished >= numRacers)
+        {
+            StopRace();
+        }
+    }
+
+    public void StartRace()
+    {
+        startButton.gameObject.SetActive(false);
+        foreach (GameObject racer in racers)
+        {
+            racer.GetComponent<Racer>().StartRace();
+        }
+    }
+
+    private void StopRace()
+    {
+        startButton.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Racer>())
+        {
+            if (other.gameObject.GetComponent<Racer>().IsFinished()) {
+                racersFinished++;
+            }
+        }
+    }
+}
